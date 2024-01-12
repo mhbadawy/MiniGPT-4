a1aea9c25a3e6f0b1063c56578b33fb4784f0aa4
Maram <maram.hesham@brightskiesinc.com>
2024-01-12 12:42:36 +0200

    enable ds config

--- minigpt4/runners/runner_base.py
+++ minigpt4/runners/runner_base.py
@@ -94,57 +94,57 @@ class RunnerBase:
         if self.dist_backend == "deepspeed":
             print('PARSED CONFIG')
             print(self.ds_config)
-            dscongiguration = {
-                "train_batch_size": 16,
-                "steps_per_print": 2000,
-                "optimizer": {
-                    "type": "Adam",
-                    "params": {
-                        "lr": 0.001,
-                        "betas": [
-                            0.8,
-                            0.999
-                        ],
-                        "eps": 1e-8,
-                        "weight_decay": 3e-7
-                    }
-                },
-                "scheduler": {
-                    "type": "WarmupLR",
-                    "params": {
-                        "warmup_min_lr": 0,
-                        "warmup_max_lr": 0.001,
-                        "warmup_num_steps": 1000
-                    }
-                },
-                "gradient_clipping": 1.0,
-                "prescale_gradients": False,
-                "bf16": {
-                    "enabled": False
-                },
-                "fp16": {
-                    "enabled": False,
-                    "fp16_master_weights_and_grads": False,
-                    "loss_scale": 0,
-                    "loss_scale_window": 500,
-                    "hysteresis": 2,
-                    "min_loss_scale": 1,
-                    "initial_scale_power": 15
-                },
-                "wall_clock_breakdown": False,
-                # "zero_optimization": {
-                #     "stage": args.stage,
-                #     "allgather_partitions": True,
-                #     "reduce_scatter": True,
-                #     "allgather_bucket_size": 50000000,
-                #     "reduce_bucket_size": 50000000,
-                #     "overlap_comm": True,
-                #     "contiguous_gradients": True,
-                #     "cpu_offload": False
-                # }
-            }
-            print('HARD-CODED CONFIG')
-            print(dscongiguration)
+            # dscongiguration = {
+            #     "train_batch_size": 16,
+            #     "steps_per_print": 2000,
+            #     "optimizer": {
+            #         "type": "Adam",
+            #         "params": {
+            #             "lr": 0.001,
+            #             "betas": [
+            #                 0.8,
+            #                 0.999
+            #             ],
+            #             "eps": 1e-8,
+            #             "weight_decay": 3e-7
+            #         }
+            #     },
+            #     "scheduler": {
+            #         "type": "WarmupLR",
+            #         "params": {
+            #             "warmup_min_lr": 0,
+            #             "warmup_max_lr": 0.001,
+            #             "warmup_num_steps": 1000
+            #         }
+            #     },
+            #     "gradient_clipping": 1.0,
+            #     "prescale_gradients": False,
+            #     "bf16": {
+            #         "enabled": False
+            #     },
+            #     "fp16": {
+            #         "enabled": False,
+            #         "fp16_master_weights_and_grads": False,
+            #         "loss_scale": 0,
+            #         "loss_scale_window": 500,
+            #         "hysteresis": 2,
+            #         "min_loss_scale": 1,
+            #         "initial_scale_power": 15
+            #     },
+            #     "wall_clock_breakdown": False,
+            #     # "zero_optimization": {
+            #     #     "stage": args.stage,
+            #     #     "allgather_partitions": True,
+            #     #     "reduce_scatter": True,
+            #     #     "allgather_bucket_size": 50000000,
+            #     #     "reduce_bucket_size": 50000000,
+            #     #     "overlap_comm": True,
+            #     #     "contiguous_gradients": True,
+            #     #     "cpu_offload": False
+            #     # }
+            # }
+            # print('HARD-CODED CONFIG')
+            # print(dscongiguration)
 
             num_parameters = 0
             p_wd, p_non_wd = [], []
@@ -165,7 +165,7 @@ class RunnerBase:
                 {"params": p_non_wd, "weight_decay": 0},
             ]
             self._wrapped_model, self._optimizer, __, __ = deepspeed.initialize(
-                model=self._model, model_parameters=optim_params, config=dscongiguration)
+                model=self._model, model_parameters=optim_params, config=self.ds_config)
         else:
             if self._model.device != self.device:
                 self._model = self._model.to(self.device)
